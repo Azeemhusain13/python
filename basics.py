650f782ea9eb0ec25c8c4cd6e103711d09e3036f
Admin <admin@Admins-MacBook-Pro.local>
2024-09-23 11:41:49 +0530

    Basics of the python

--- basics.py
+++ basics.py
@@ -2,3 +2,27 @@ print("day1")
 def sum(a,b):
     return a+b
 print(sum(1,4))
+
+print ("hello")
+print("Hello \"world\"")
+print(round(2**0.5,4))
+name ="Harry"
+print(name)
+surName ="Joe"
+print(name+" "+surName + " 3")
+print("surName" *3)
+# name1= input("enter your name")
+# name2=input("enter your age")
+# print(name1)
+# name3,age=input("enter your name and age").split()
+# print("hello {} your age is {}".format(name3,age))
+# print(name3)
+# print(age)
+print(2 + 4)
+print(2-4)
+print(2*4)
+print(type(2/4))
+print(type(0))
+print(9.3+9.8)
+print(2//4)
+print(2**4)
